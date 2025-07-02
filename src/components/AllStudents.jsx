1c52001550a333adddbe2de146e1d3e5206f0373
Darrell Moreno <dmoreno1987@outlook.com>
2025-07-02 14:39:00 -0400

    AllStudents and StudentCard created

--- src/components/AllStudents.jsx
+++ src/components/AllStudents.jsx
@@ -1,1 +1,29 @@
 //Include a delete button
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const AllStudents = ({ students, fetchAllStudents }) => {
+  return (
+    <div>
+      <h2> Students </h2>
+      {students.length > 0 ? (
+        students.map((student) => (
+          <StudentCard
+            key={student.id}
+            student={student}
+            fetchAllStudents={fetchAllStudents}
+          />
+        ))
+      ) : (
+        <p>No Students found</p>
+      )}
+      <Link to="/add-students" className="add-btn">
+        {" "}
+        Add Student{" "}
+      </Link>
+    </div>
+  );
+};
+
+export default AllStudents;
